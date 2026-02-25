e09521701a03893acddf357360fa78f5388bc177
Manasj06 <joshi.man6002@gmail.com>
2026-02-25 11:28:17 +0530

    Updated Palindrome_dequeue with palindrome logic

---
+++ src/Palindrome_dequeue.java
@@ -0,0 +1,38 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Palindrome_dequeue {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
